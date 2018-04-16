b28ec4191d1301fba09d88162db7fe18a0f7debd
frankievalentine <frankievalentine9@gmail.com>
2018-04-16 16:51:21 -0700

    Added functionality to add newItem to data set and UI

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
 // Budget Controller
 var budgetController = (function() {
 
-    // some code
+    // Private function constructors for data of Expense and Income
+    // First letter capitalized
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    // Store both Expense and Income into one data structure
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    // Create public access to data methods
+    return {
+        // Income or Expense, description, value
+        // type from getInput object
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // new ID for newItem
+            // ID = last ID + 1
+            // type will be 'inc' or 'exp'
+            // if allItems object is > 0 locate the last position of [type] array
+            // otherwise the ID is 0
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // push to data set allItems object
+            data.allItems[type].push(newItem);
+            // return new element so new data created has access publicly
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -13,7 +73,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     // Public function that iffy returns with object assigned to UIController
@@ -28,6 +90,28 @@ var UIController = (function() {
         // Expose selector strings to global scope
         getDOMstrings: function() {
             return DOMstrings;
+        },
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline" /></button></div></div></div>';
+            } else if (type == 'exp') {
+                element = DOMstrings.expensesContainer;
+                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline" /></button></div></div></div>';
+            }
+            // Replace placeholder text with data
+            // id property holds the id
+            newHtml = html.replace('%id%', obj.id);
+            // newHtml is now the id
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert HTML into DOM (using incertAdjacent method)
+            // Child's of income__list and expense__list
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         }
     };
 })();
@@ -53,13 +137,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // Used in both event listeners, requires seperate function
     var ctrlAddItem = function() {
+        var input, newItem;
+
         // Get field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
-        // Add item to budget controller
-        
+        // Add item to budget controller (addItem accepts three parameters)
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         // Add new item to UI
-
+        UICtrl.addListItem(newItem, input.type);
         // Calculate the budget
 
         // Display budget to UI
